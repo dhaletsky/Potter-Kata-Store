76f14b5ddce61f4c6e3a7aff6a434bf4bb86a979
Dzmitry Khaletski <d.khaletski@godeltech.com>
2017-05-25 12:24:15 +0300

    Add DiscountService tests

---
+++ src/PotterKata/PotterKata.Tests/Services/DiscountServiceTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterKata.Business.Services;
+using PotterKata.Models.Books;
+
+namespace PotterKata.Tests.Services
+{
+    [TestClass]
+    public class DiscountServiceTests
+    {
+        private IDiscountService service;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            service = new DiscountService();
+        }
+
+        [TestMethod]
+        public void CalculateDiscount_ThreeDifferentGroupBooks()
+        {
+            var orderedBooks = new List<HarryPotterBook>
+            {
+                new HarryPotterBook {Type = BookTypes.TheChamberOfSecrets},
+
+                new HarryPotterBook {Type = BookTypes.TheGobletOfFire},
+                new HarryPotterBook {Type = BookTypes.TheGobletOfFire},
+
+                new HarryPotterBook {Type = BookTypes.TheOrderofThePhoenix},
+                new HarryPotterBook {Type = BookTypes.TheOrderofThePhoenix}
+            };
+
+            Assert.AreEqual(2.4m, service.CalculateDiscount(orderedBooks));
+        }
+
+        [TestMethod]
+        public void CalculateDiscount_OneBook()
+        {
+            var orderedBooks = new List<HarryPotterBook>
+            {
+                new HarryPotterBook {Type = BookTypes.TheChamberOfSecrets}
+            };
+
+            Assert.AreEqual(0m, service.CalculateDiscount(orderedBooks));
+        }
+
+        [TestMethod]
+        public void CalculateDiscount_AllSameBaooksInGroup()
+        {
+            var orderedBooks = new List<HarryPotterBook>
+            {
+                new HarryPotterBook {Type = BookTypes.TheChamberOfSecrets},
+                new HarryPotterBook {Type = BookTypes.TheChamberOfSecrets},
+                new HarryPotterBook {Type = BookTypes.TheChamberOfSecrets},
+                new HarryPotterBook {Type = BookTypes.TheChamberOfSecrets},
+            };
+
+            Assert.AreEqual(0m, service.CalculateDiscount(orderedBooks));
+        }
+
+        [TestMethod]
+        public void CalculateDiscount_AllSeriesOfBooks()
+        {
+            var orderedBooks = new List<HarryPotterBook>
+            {
+                new HarryPotterBook {Type = BookTypes.TheChamberOfSecrets},
+                new HarryPotterBook {Type = BookTypes.TheDeathlyHallows},
+                new HarryPotterBook {Type = BookTypes.TheGobletOfFire},
+                new HarryPotterBook {Type = BookTypes.TheHalfBloodPrince},
+                new HarryPotterBook {Type = BookTypes.TheOrderofThePhoenix},
+                new HarryPotterBook {Type = BookTypes.ThePhilosopherStone},
+                new HarryPotterBook {Type = BookTypes.ThePrisonerOfAzkaban},
+            };
+
+            Assert.AreEqual(19.6m, service.CalculateDiscount(orderedBooks));
+        }
+    }
+}
