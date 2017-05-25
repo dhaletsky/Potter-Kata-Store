062444f61e20b72b3758d3827c07fb7ad5b8fa19
Dzmitry Khaletski <d.khaletski@godeltech.com>
2017-05-25 12:34:57 +0300

    Add InterestService implementation

--- src/PotterKata/PotterKata.Business/Services/InterestService.cs
+++ src/PotterKata/PotterKata.Business/Services/InterestService.cs
@@ -1,10 +1,14 @@
-﻿namespace PotterKata.Business.Services
+﻿using System;
+
+namespace PotterKata.Business.Services
 {
     public class InterestService : IInterestService
     {
         public decimal CalculatePercantage(ushort percentageDiscount)
         {
-            throw new System.NotImplementedException();
+            if (percentageDiscount > 100) throw new ArgumentOutOfRangeException("percentageDiscount", "Can`t be more than 100%");
+
+            return (decimal)percentageDiscount / 100;
         }
     }
 }
