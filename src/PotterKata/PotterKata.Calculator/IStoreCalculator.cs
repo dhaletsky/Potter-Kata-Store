81f2758579bc581b08ac3d8e39cf4999a5f88869
Dzmitry Khaletski <d.khaletski@godeltech.com>
2017-05-25 13:16:53 +0300

    Add IStoreCalculator abstraction

---
+++ src/PotterKata/PotterKata.Calculator/IStoreCalculator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using PotterKata.Models.Books;
+
+namespace PotterKata.Calculator
+{
+    public interface IStoreCalculator
+    {
+        decimal Calculate(IList<HarryPotterBook> orderedBooks);
+    }
+}
---
+++ src/PotterKata/PotterKata.Calculator/StoreCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using PotterKata.Models.Books;
+
+namespace PotterKata.Calculator
+{
+    public class StoreCalculator : IStoreCalculator
+    {
+        public decimal Calculate(IList<HarryPotterBook> orderedBooks)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
